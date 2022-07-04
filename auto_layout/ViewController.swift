e1d2aaa2257495caaa6f8707cbee20f49f2148ae
anadzebniauri <ana.dzebniauri@space.ge>
2022-07-04 22:28:59 +0400

    Initial Commit

---
+++ auto_layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  auto_layout
+//
+//  Created by Ana Dzebniauri on 04.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
